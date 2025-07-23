8caff069ce62190a73fa6ae2e67eef4144b0b86a
Abhijeet Kumar <abhijeet.kumar@abhijeetvrs-33.successivetech.com>
2025-07-23 13:32:22 +0530

    Fixed the list file

--- src/routes/mockRoute.ts
+++ src/routes/mockRoute.ts
@@ -3,14 +3,16 @@ import { mockController } from "../controller/mockController.js";
 import authenticateJWT from "../middleware/auth.js";
 import { rateLimiterMiddleware } from "../middleware/rateLimiter.js";
 
+
 const router = Router();
 
 router.get("/mockData", authenticateJWT, mockController);
 router.post("/mockData", rateLimiterMiddleware(3, 6), authenticateJWT, mockController);
 
 
+
 router.get("/mocklist", authenticateJWT, (req: Request, res: Response) => {
-  res.sendFile(__dirname + "/../utils/mockList.ts");
+  res.sendFile("/home/abhijeet.kumar/Documents/training/backend-training/src/utils/mockList.ts");
 });
 
 export default router;
--- src/utils/mockList.ts
+++ src/utils/mockList.ts
@@ -1,4 +1,4 @@
-export const mockList= [
+[
   { id: 1, name: "Ajay", address: "Noida" },
   { id: 2, name: "Varun", address: "Delhi" },
  
