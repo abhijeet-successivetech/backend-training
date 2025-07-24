ec3c730106fe5c9df5b1f5f64438f8c665bd810d
Abhijeet Kumar <abhijeet.kumar@abhijeetvrs-33.successivetech.com>
2025-07-24 17:44:37 +0530

    added assignment 9

---
+++ src/controller/StudentController.ts
@@ -0,0 +1,31 @@
+// src/controllers/StudentController.ts
+
+import { Request, Response } from "express";
+import StudentModel, { IStudent } from "../models/Student";
+
+export default class StudentController {
+  async createStudent(req: Request, res: Response): Promise<void> {
+    try {
+      const { name, city, age } = req.body;
+      if (!name || !city || typeof age !== "number") {
+        res.status(400).json({ status: false,
+             message: "Name, city, and age are required." });
+        return;
+      }
+
+      const student = await StudentModel.create({ name, city, age });
+
+      res.status(201).json({
+        status: true,
+        data: student,
+        message: "Student created successfully"
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: false,
+        message: "Error creating student",
+      
+      });
+    }
+  }
+}
